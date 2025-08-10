c5053d231148bc80b0e9b8a75213cd1012ebaee8
Yonatan Brauner <yonabrauner@gmail.com>
2025-08-10 13:56:54 +0300

    changed tsconfig file for correct db behaviour

--- dist/db.js
+++
@@ -1,6 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-export const prisma = new PrismaClient();
-// Optional: Log connection success (for debugging)
-prisma.$connect()
-    .then(() => console.log("Connected to PostgreSQL via Prisma"))
-    .catch((err) => console.error("Prisma connection error:", err));
